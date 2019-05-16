fa8939da8635c7952f6655a0c8b1036d93c33683
zhang211233 <1426368128@qq.com>
2019-05-16 09:30:03 +0800

    提交到本地仓库

---
+++ student_prc/src/com/student/model/StudentModel.java
@@ -0,0 +1,47 @@
+package com.student.model;
+
+public class StudentModel {
+	private int id;
+	private int sex;
+	private String username;
+	private String useremail;
+	private String  love;
+	@Override
+	public String toString() {
+		return "StudentModel [id=" + id + ", sex=" + sex + ", username=" + username + ", useremail=" + useremail
+				+ ", love=" + love + "]";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StudentModel other = (StudentModel) obj;
+		if (id != other.id)
+			return false;
+		if (love == null) {
+			if (other.love != null)
+				return false;
+		} else if (!love.equals(other.love))
+			return false;
+		if (sex != other.sex)
+			return false;
+		if (useremail == null) {
+			if (other.useremail != null)
+				return false;
+		} else if (!useremail.equals(other.useremail))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	
+	
+}
